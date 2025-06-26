2247de95a0cea128ded5337a8e84b17eb429836b
mattrahendra <ismadirahendra@gmail.com>
2025-06-26 17:22:38 +0700

    Add form request classes for storing and updating categories and templates

---
+++ app/Http/Requests/UpdateTemplateRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTemplateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'category_id' => 'sometimes|required|exists:categories,id',
+            'price' => 'sometimes|required|numeric|min:0',
+        ];
+    }
+}
